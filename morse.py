778804787de3d73f45f4bbe2720c5a8e74553734
FrancescoMusi <francesco.musi03@gmail.com>
2018-10-16 23:08:04 +0200

    finished

--- morse.py
+++ morse.py
@@ -1,56 +1,6 @@
 import sys
-alfabetoMorse = {
-	
-	"a" : ".-",
-	"b" : "-...",
-	"c" : "-.-.",
-	"d" : "-..",
-	"e" : ".",
-	"f" : "..-.",
-	"g" : "--.",
-	"h" : "....",
-	"i" : "..",
-	"j" : ".---",
-	"k" : "-.-",
-	"l" : ".-..",
-	"m" : "-.",
-	"n" : "-.",
-	"o" : "---",
-	"p" : ".--." ,
-	"q" : "--.-",
-	"r" : ".-." ,
-	"s" : "...", 
-	"t" : "-",
-	"u" : "..-",
-	"v" : "...-", 
-	"w" : ".--" ,
-	"x" : "-..-",
-	"y" : "-.--",
-	"z" : "--..",
-	"0" : "-----",
-	"1" : ".----",
-	"2" : "..---",
-	"3" : "...--",
-	"4" : "....-",
-	"5" : ".....",
-	"6" : "-....",
-	"7" : "--...",
-	"8" : "---..",
-	"9" : "----.",
-	"." : ".-.-.-",
-	"," : "--..--",
-	":" : "---...",
-	"?" : "..--..",
-	"=" : "-...-",
-	"-" : "-....-",
-	"(" : "-.--.",
-	")" : "-.--.-",
-	"/" : "-..-.",
-	"@" : ".--.-.",
-	"!" : "-.-.--",
+from obj import alfabetoMorse, morseAlfabeto
 
-	
-}
 while True:
 	while True:
 		print("morse-alfabeto(1) o alabeto-morse(2)?")
@@ -61,47 +11,39 @@ while True:
 				print("inserisci la lettera/parola/frase: (invio per uscire)")
 				parola = input()
 				lp = len(parola)
-				
+				p = ""
 				if parola != "":
 					for i in parola:
 						if i == " ":
 							lp -= 1
-							print("|", end="")
+							p += "|"
 						for x in alfabetoMorse:
 							if x == i:
 								lp -= 1
 								if lp == 0:
-									print(alfabetoMorse[str(x)])
+									p += alfabetoMorse[str(x)]
 								else: 
-									print(alfabetoMorse[str(x)], end="|")
-
+									p += alfabetoMorse[str(x)] + "|"
+					print(p)
 				else: 
 					sys.exit()
 
 		elif z == "1":
-
 			while True:
 				print()
-				print("inserisci il morse mettendo uno spazio tra le lettere e | tra le parole: (invio per uscire)")
+				print("inserisci il morse mettendo una | tra le lettere e || tra le parole: (invio per uscire)")
 				parola = input()
-				p = " "
+				p = ""
 				if parola != "":
+					parola = parola.split("|")
 					for i in parola:
-						if i == "|":
-							print(" ", end="")
+						if i == "":
+							p += " "
 						else:
-							if i != " ":
-								p.join(i)	
-								print(p, end="")
-							#else:
-								#for x in alfabetoMorse:
-									#if x == p:
-										#print(x, end="")	
-									#else:
-										#print("la lettera", p, "non esiste.")				
+							p += morseAlfabeto[i]
+					print(p)
 				else: 
 					sys.exit()
-
 		else:
 			print("!!!inserisci 1 o 2!!!")
 			break
